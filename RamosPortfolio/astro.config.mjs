179bb967812c0dffd099afa3094335d9c546a706
Ramos <joaopedroramos1512@gmail.com>
2026-04-19 06:18:25 -0300

    Deploy test

--- RamosPortfolio/astro.config.mjs
+++ RamosPortfolio/astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://ramos902.github.io',
-  base: import.meta.env.PROD ? '/Portfolio' : '/',
+  base: '/Portfolio',
 });
