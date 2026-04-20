3b5ff07383bc79336da78e375a04c0e64b6b5b40
Ramos <joaopedroramos1512@gmail.com>
2026-04-20 03:01:04 -0300

    Feature: Fixing bugs in blog

--- RamosPortfolio/src/content/blog/config.ts
+++ RamosPortfolio/src/content/blog/config.ts
@@ -1,12 +1,13 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.date(),
-    tags: z.array(z.string()).optional(),
-  }),
+    schema: z.object({
+        postId: z.number(),
+        title: z.string(),
+        description: z.string(),
+        date: z.date(),
+        tags: z.array(z.string()).optional(),
+    }),
 });
 
 export const collections = { blog };
--- RamosPortfolio/src/content/blog/firstposttest.md
+++ RamosPortfolio/src/content/blog/firstposttest.md
@@ -1,4 +1,5 @@
 ---
+postId: 1
 title: "Como eu uso PostgreSQL no dia a dia"
 description: "Índices, queries e o que aprendi no HLOB"
 date: 2025-04-20
